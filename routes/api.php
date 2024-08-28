30e3b4622dc114459c951fc00115c32a1eea9099
maartins-dudaa <161869560+maartins-dudaa@users.noreply.github.com>
2024-08-28 15:45:47 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,16 @@ use Illuminate\Support\Facades\Route;
 
 //Rota de gravação-gravar algo novo para o Banco 
 Route::post('/usuario', [UsuarioControler::class, 'store']);
+
+
+Route::get('/usuario/{id}/find', [UsuarioControler::class, 'findById']);
+
+Route::get('/usuario', [UsuarioControler::class, 'index']);
+
+Route::get('/usuario/search', [UsuarioControler::class, 'searchByName']);
+
+Route::get('/usuario/search/email', [UsuarioControler::class, 'searchByEmail']);
+
+Route::delete('/usuario/{id}/delete', [UsuarioControler::class, 'delete']);
+
+Route::put('/usuario', [UsuarioControler::class, 'update']);
