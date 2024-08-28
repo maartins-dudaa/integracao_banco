c1ad3c641427e1137a6df691ff9ee35d7569c9a2
maartins-dudaa <161869560+maartins-dudaa@users.noreply.github.com>
2024-08-28 15:46:12 -0300

    Update UsuarioControler.php

--- app/Http/Controllers/UsuarioControler.php
+++ app/Http/Controllers/UsuarioControler.php
@@ -2,33 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuarios;
 use App\Service\UsuarioService;
 use Illuminate\Http\Request;
 
 class UsuarioControler extends Controller
 
 
+{
+    protected $usuarioService;
+
+
+
+    public function  __construct(UsuarioService $novoUsuarioService)
+
+
     {
-        protected $usuarioService;
+
+        $this->usuarioService = $novoUsuarioService;
+    }
+
+    public function store(Request $request)
+    {
+        $user = $this->usuarioService->create($request->all());
+
+        return $user;
+    }
 
 
-        
-        public function  __construct(UsuarioService $novoUsuarioService)
-       
-        
-        {
+    public function findById($id)
+    {
+        $result = $this->usuarioService->findById($id);
+        return response()->json($result); 
+    }
 
-            $this->usuarioService = $novoUsuarioService;
-           
-        }
+    public function index(){
+        $result = $this->usuarioService->getAll();
+        return response()->json($result);
+    }
 
-        public function store(Request $request)
-        {
-            $user = $this->usuarioService->create($request->all());
 
-            return $user;
-        }
+    public function searchByEmail(Request $request){
+        $result = $this->usuarioService->searchByEmail($request->email);
+        return response()->json($result);
 
-    };
+    }
 
-    
+
+
+    public function delete($id){
+        $result = $this->usuarioService->delete($id);
+        $usuario = Usuarios::find($id);
+        if($usuario == null){
+            return [
+                'status' => false,
+                'message' => 'Usuário não encontrado'
+            ];
+        } 
+        $usuario->delete();
+        return [ 
+            'status' => true,
+            'message' => 'Usuário excluido com sucesso'
+        ];
+
+
+
+
+
+    }
+
+
+
+    public function update(Request $request){
+        $result = $this->usuarioService->update($request->all());
+        return response()->json($result);
+
+    }
+
+
+};
